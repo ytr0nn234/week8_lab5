7c236dc68ee67bd27bf7fc6234e8c42ee706c777
ytr0nn234 <g00467242@atu.ie>
2026-03-09 11:53:15 +0000

    Initial

---
+++ src/main/java/com/lab5/week8_lab5/Week8Lab5Application.java
@@ -0,0 +1,13 @@
+package com.lab5.week8_lab5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week8Lab5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Week8Lab5Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/lab5/week8_lab5/Week8Lab5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lab5.week8_lab5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week8Lab5ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
